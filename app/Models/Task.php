0468ac317398f230187d902bde1d64a29e62599f
Carlos Manuel Vargas <cmv-66@hotmail.com>
2026-07-15 21:02:15 -0300

    feat: create controler and models for task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'completed', 'manager_id'];
+
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(Manager::class);
+    }
 }
---
+++ app/Rules/ProhibitedWords.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class ProhibitedWords implements ValidationRule
+{
+
+    protected array $forbiddenWords = [
+        'spam',
+        'fake',
+        'prohibited'
+    ];
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  Closure(string, ?string=): PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        foreach($this->forbiddenWords as $word){
+            if(stripos($value, $word) !== false){
+                $fail("El campo {$attribute} contiene palabras prohibidas");
+                return;
+            }
+        }
+    }
+}
